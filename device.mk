77d7dacac10d33c7d4738d289994f4a94c52636d
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2024-01-31 04:20:15 +0000

    topaz: Add XiaomiParts

--- device.mk
+++ device.mk
@@ -402,6 +402,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
