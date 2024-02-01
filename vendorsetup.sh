98f70656438e1834c03646e55b187d099a3f6c57
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2024-02-01 05:36:42 +0000

    topaz: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/topaz-area/device_xiaomi_topaz-kernel.git -b lineage-21 device/xiaomi/topaz-kernel;
+git clone https://github.com/topaz-area/vendor_xiaomi_topaz.git -b lineage-21 vendor/xiaomi/topaz;
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi;
