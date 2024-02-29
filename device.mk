d879ec889d4ac98236a7cae41e2f54c619f18e79
sirNewbies <yusromi04@gmail.com>
2024-02-29 13:41:39 +0700

    topaz: Set frame rate multiple threshold to 120

--- device.mk
+++ device.mk
@@ -374,6 +374,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Surfaceflinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.frame_rate_multiple_threshold=120
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
